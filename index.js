0835307ba13d149d7d9ca36e6732054c6d387bb8
tsuzakitakehiro <tsuzakitakehiro@team-lab.com>
2020-09-10 04:14:52 +0900

    修正

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const protect = require('static-auth')
 const safeCompare = require('safe-compare')
 
-/*
- *
- */
+const USER_NAME = process.env.USER_NAME || 'user'
+const PASSWORD = process.env.PASSWORD || 'password'
 
-const app = protect('/', (username, password) => safeCompare(username, 'admin') && safeCompare(password, 'admin'), {
-  directory: __dirname + '/dist',
+const app = protect('/', (username, password) => safeCompare(username, USER_NAME) && safeCompare(password, PASSWORD), {
+  directory: `${__dirname}/public`,
   realm: 'now-basic-auth.node-static-auth',
   onAuthFailed: res => {
-    res.end('Restricted area, please login (admin:admin).')
+    res.end('Authentication failed')
   }
 })
 
