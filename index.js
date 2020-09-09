df8b5bfc0eae33ec7b3e8325b725affbf0cf556e
tsuzakitakehiro <tsuzakitakehiro@team-lab.com>
2020-09-10 04:04:49 +0900

    修正

--- index.js
+++ index.js
@@ -1,40 +1,16 @@
-const readAuth = require('basic-auth')
+const protect = require('static-auth')
 const safeCompare = require('safe-compare')
-const serveStatic = require('serve-static')
 
 /*
  *
  */
 
-const auth = req =>
-  new Promise(resolve => {
-    const credentials = readAuth(req)
-    const authorized = credentials && safeCompare(credentials.name, 'admin') && safeCompare(credentials.pass, 'admin')
-    resolve(authorized)
-  })
-
-const serveHandler = serveStatic(__dirname + '/public')
-const serve = (req, res, handle404) => new Promise(() => serveHandler(req, res, handle404))
-
-/*
- *
- */
-
-const app = async (req, res) => {
-  // If requests admin area, auth user before serving files
-  if (req.url.startsWith('/')) {
-    const authorized = await auth(req, res)
-    if (!authorized) {
-      res.writeHead(401, { 'WWW-Authenticate': 'Basic realm="now-basic-auth.node"' })
-      return res.end('Restricted area, please login (admin:admin).')
-    }
+const app = protect('/', (username, password) => safeCompare(username, 'admin') && safeCompare(password, 'admin'), {
+  directory: __dirname + '/dist',
+  realm: 'now-basic-auth.node-static-auth',
+  onAuthFailed: res => {
+    res.end('Restricted area, please login (admin:admin).')
   }
-
-  // Serve files
-  return serve(req, res, () => {
-    res.statusCode = 404
-    res.end('404 Not Found')
-  })
-}
+})
 
 module.exports = app
